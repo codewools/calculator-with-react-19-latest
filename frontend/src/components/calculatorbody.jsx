5f7d35655dfabd0f5eb4329ad9bfea55f1c36ce4
M Hamza <129577533+codewools@users.noreply.github.com>
2025-01-25 17:15:52 +0500

    Update calculatorbody.jsx

--- frontend/src/components/calculatorbody.jsx
+++ frontend/src/components/calculatorbody.jsx
@@ -3,7 +3,7 @@ import { useState ,useEffect} from 'react'
 function Calculatorbody() {
   const [number, setNumber] = useState("");
   const [result, setResult] = useState("");
-const arrOfNumbers =["AC", "%" , "7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "C", "="]
+const arrOfNumbers =["AC", "%" , "C", "7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "="]
 const arrOfExpressions = ["/",'**', "*", "-", "+"];
 const [historyOfResults, setHistoryOfResults] = useState([]);
 const handler = async (e) => {
@@ -36,7 +36,7 @@ const handler = async (e) => {
   }, []);
   return (
     <div className="flex items-center justify-center bg-gray-800">
-      <div className="border border-solid p-8 rounded shadow-xl w-110 ">
+      <div className="border border-solid p-8 rounded-3xl shadow-xl w-110 bg-white ">
         {/* view history  */}
         <div className='bg-gray-100 text-gray-500 h-20 w-full text-2xl shadow-xl flex items-center justify-end'>
         {historyOfResults.slice(-1).map((item) => (
@@ -47,11 +47,11 @@ const handler = async (e) => {
         <div className='bg-gray-100 h-15 w-full text-2xl shadow-xl flex items-center justify-end'>
           <h1 className='text-3xl'>{result ? result : number}</h1>
         </div>
-        <div className='flex justify-between bg-gray-200'>
+        <div className='flex justify-between bg-gray-200  rounded-bl-3xl'>
           {/* numericals values */}
           <div className='w-70 bg-gray-200 h-115 p-2 '>
             {arrOfNumbers.map((item) => (
-              <p key={item} onClick={handler} className={` ${item === "AC" ? "bg-red-500 " : " bg-white"} float-left text-xl mr-2 mt-2 cursor-pointer rounded-full shadow-xl items-center justify-center w-20 h-20 flex  `}>
+              <p key={item} onClick={handler} className={` ${item === "AC" ? "bg-red-500 " : " bg-white"} float-left text-3xl mr-2 mt-2 cursor-pointer rounded-full shadow-xl items-center justify-center w-20 h-20 flex  `}>
                 {item}
               </p>
             ))}
@@ -61,7 +61,7 @@ const handler = async (e) => {
           <div className='mt-2'>
               {
                 arrOfExpressions.map((item) => (
-                  <p key={item} onClick={handler} className=' shadow-xl mr-2 cursor-pointer text-xl mt-2    rounded-full  items-center justify-center w-20 h-20 bg-sky-300 flex'>{item}</p> 
+                  <p key={item} onClick={handler} className=' shadow-xl mr-2 cursor-pointer text-3xl mt-2    rounded-full  items-center justify-center w-20 h-20 bg-sky-300 flex'>{item}</p> 
            
                 ))
               }
